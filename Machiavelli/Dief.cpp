1c4343da9c2d5dcc69ff64502346f6aa2d98abda
Yannik Hegge <yannikhegge@gmail.com>
2016-01-20 15:15:57 +0100

    Dief fixed

--- Machiavelli/Dief.cpp
+++ Machiavelli/Dief.cpp
@@ -15,17 +15,32 @@ Dief::~Dief()
 
 bool Dief::execute(ClientCommand command, std::shared_ptr<GameHandler> game) const
 {
+
+	std::vector<int> characterIds;
+	for (int i = 3; i < 8; i++) {
+		//Begint bij 2 om de moordenaar en dief over te slaan
+		const Character& chr = game->getCharacter(i);
+		if (!chr.isKilled()) {
+			characterIds.push_back(chr.getId());
+		}
+	}
 	if (!command.is_number()) {
+		owner->get_socket()->write("Ongeldige input \r\n");
 		return false;
 	}
 	else {
 		int stolen = stoi(command.get_cmd());
-		if (stolen > 0 && stolen < charactersToSteal.size()) {
-			game->stealCharacter(charactersToSteal[stolen - 1], command.get_player());
+		if (stolen == 0)
+		{
+			return true;
+		}
+		else if (stolen > 0 && stolen <= characterIds.size()) {
+			game->stealCharacter(characterIds[stolen - 1], command.get_player());
 			return true;
 		}
 		else {
 			//Geen geldige steal
+		owner->get_socket()->write("Ongeldige input 2 \r\n");
 			return false;
 		}
 	}
@@ -33,13 +48,16 @@ bool Dief::execute(ClientCommand command, std::shared_ptr<GameHandler> game) con
 
 std::string Dief::getActionDescription(std::shared_ptr<GameHandler> game) const {
 	std::string s = "Kies iemand om te bestelen: \r\n";
+	s += "[0] niemand: \r\n";
 	std::vector<int> characterIds;
+	int j = 1;
 	for (int i = 3; i < 8; i++) {
 		//Begint bij 2 om de moordenaar en dief over te slaan
 		const Character& chr = game->getCharacter(i);
 		if (!chr.isKilled()) {
 			characterIds.push_back(chr.getId());
-			s += "[" + std::to_string(i - 2) + "] " + chr.getName() + "\r\n";
+			s += "[" + std::to_string(j) + "] " + chr.getName() + "\r\n";
+			j++;
 		}
 	}
 	return s;
